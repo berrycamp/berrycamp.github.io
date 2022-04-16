9f4e3d987b7f6c1380281ed613f79f94c2d55c93
Sean Martin <29813268+martin-sean@users.noreply.github.com>
2022-04-16 21:30:04 +1000

    Capitalise C in metadata title

--- logic/utils/title.ts
+++ logic/utils/title.ts
@@ -12,4 +12,4 @@ export const getTitle = (title: string): string => `camp · ${title}`;
  * @param title Page title.
  * @returns A title suitable for the webpage.
  */
-export const getMetadataTitle = (title: string): string => `Berry ${getTitle(title)}`;
+export const getMetadataTitle = (title: string): string => `Berry Camp · ${title}`;
