63f4e3e4ebfc29100f1b68d94dfa9ddfb9058304
Sean Martin <29813268+martin-sean@users.noreply.github.com>
2022-04-26 23:12:30 +1000

    Render entire layout only when mounted

--- modules/layout/Layout.tsx
+++ modules/layout/Layout.tsx
@@ -36,79 +36,83 @@ export const Layout: FC<LayoutProps> = ({title, description, image, children}) =
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <main>
-        <AppBar position="sticky">
-          <Toolbar
-            variant="dense"
-            sx={{
-              "&.MuiToolbar-root": {
-                paddingLeft: 0,
-                paddingRight: 0.5,
-              }
-            }}
-          >
-            <Box flexGrow={1}>
-              <Box height={48} width={288} position="relative">
-                <Link passHref href="/">
-                  <a style={{position: "relative", display: "flex", alignItems: "center", width: "100%", height: "100%"}}>
-                    <Box sx={{position: "relative", width: "100%", height: "100%", display: {xs: "none", sm: "block"}}}>
-                      <Image
-                        unoptimized
-                        priority
-                        src={'/img/logo.png'}
-                        alt='Animation of madeline in a campsite in game'
-                        layout="fill"
-                        style={{
-                          imageRendering: "pixelated",
-                        }}
-                      />
-                    </Box>
-                    <Box display="flex" position="absolute" left={12}>
-                      <Typography
-                        sx={{
-                          fontSize: 28,
-                          paddingRight: 1,
-                        }}
-                      >
-                        <span role='img' aria-label='Berry'>🍓</span>
-                      </Typography>
-                      <Typography
-                        component="div"
-                        sx={{
-                          letterSpacing: 4,
-                          color: "white",
-                          fontSize: 28,
-                          textShadow: "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
-                        }}
-                      >
-                        camp
-                      </Typography>
-                    </Box>
-                  </a>
-                </Link>
-              </Box>
-            </Box>
-            <SettingsMenu />
-          </Toolbar>
-        </AppBar>
-        {settings.cozyMode ? (
+        {mounted && (
           <Fragment>
-            <Box position="fixed" bottom={0} zIndex={-1} width="100%" height="100%">
-              <Image
-                unoptimized
-                priority
-                objectFit="cover"
-                src={`${COZY_IMAGE_URL}/${Math.floor(Math.random() * COZY_IMAGE_COUNT) + 1}.png`}
-                alt='A large comfy animation of Madeline near a campfire in-game'
-                layout="fill"
-                style={{
-                  imageRendering: "pixelated",
+            <AppBar position="sticky">
+              <Toolbar
+                variant="dense"
+                sx={{
+                  "&.MuiToolbar-root": {
+                    paddingLeft: 0,
+                    paddingRight: 0.5,
+                  }
                 }}
-              />
-            </Box>
-          </Fragment>
-        ) : (
-          <Fragment>
-            {mounted && children}
+              >
+                <Box flexGrow={1}>
+                  <Box height={48} width={288} position="relative">
+                    <Link passHref href="/">
+                      <a style={{position: "relative", display: "flex", alignItems: "center", width: "100%", height: "100%"}}>
+                        <Box sx={{position: "relative", width: "100%", height: "100%", display: {xs: "none", sm: "block"}}}>
+                          <Image
+                            unoptimized
+                            priority
+                            src={'/img/logo.png'}
+                            alt='Animation of madeline in a campsite in game'
+                            layout="fill"
+                            style={{
+                              imageRendering: "pixelated",
+                            }}
+                          />
+                        </Box>
+                        <Box display="flex" position="absolute" left={12}>
+                          <Typography
+                            sx={{
+                              fontSize: 28,
+                              paddingRight: 1,
+                            }}
+                          >
+                            <span role='img' aria-label='Berry'>🍓</span>
+                          </Typography>
+                          <Typography
+                            component="div"
+                            sx={{
+                              letterSpacing: 4,
+                              color: "white",
+                              fontSize: 28,
+                              textShadow: "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+                            }}
+                          >
+                            camp
+                          </Typography>
+                        </Box>
+                      </a>
+                    </Link>
+                  </Box>
+                </Box>
+                <SettingsMenu />
+              </Toolbar>
+            </AppBar>
+            {settings.cozyMode ? (
+              <Fragment>
+                <Box position="fixed" bottom={0} zIndex={-1} width="100%" height="100%">
+                  <Image
+                    unoptimized
+                    priority
+                    objectFit="cover"
+                    src={`${COZY_IMAGE_URL}/${Math.floor(Math.random() * COZY_IMAGE_COUNT) + 1}.png`}
+                    alt='A large comfy animation of Madeline near a campfire in-game'
+                    layout="fill"
+                    style={{
+                      imageRendering: "pixelated",
+                    }}
+                  />
+                </Box>
+              </Fragment>
+            ) : (
+              <Fragment>
+                {children}
+              </Fragment>
+            )}
           </Fragment>
         )}
       </main>
