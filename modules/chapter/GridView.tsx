595d0e6398144813e751ced619ead89b1ae8a129
Sean Martin <29813268+wishcresp@users.noreply.github.com>
2023-02-05 12:32:03 +1100

    Add side pages

--- modules/chapter/GridView.tsx
+++ modules/chapter/GridView.tsx
@@ -1,6 +1,8 @@
 import {Card, CardActionArea, CardMedia, Grid, ImageListItemBar} from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 import {FC} from "react";
+import {AspectBox} from "../common/aspectBox/AspectBox";
 import {getRoomPreviewUrl} from "../fetch/dataApi";
 import {ChapterViewItemProps, ChapterViewProps} from "./types";
 
@@ -18,34 +20,39 @@ export const ChapterGridView: FC<ChapterViewProps> = ({areaId, chapterId, sideId
   </Grid>
 );
 
-export const ChapterGridItem: FC<ChapterViewItemProps> = ({roomId, roomName, href, image})=> (
-  <Grid item xs={12} sm={6} md={4}>
-    <Card sx={{width: "100%", height: "100%"}}>
-      <Link passHref href={href}>
-        <CardActionArea
-          sx={{flexGrow: 1, flexDirection: "column", alignItems: "stretch", height: "100%"}}
-          style={{
-            imageRendering: "pixelated",
-          }}
-        >
-          <CardMedia
-            component="img"
-            src={image}
-            alt={`Thumbnail for room ${roomName}`}
+export const ChapterGridItem: FC<ChapterViewItemProps> = ({roomId, roomName, href, image}) => {
+  return (
+    <Grid item xs={12} sm={6} md={4}>
+      <Card>
+        <Link passHref href={href}>
+          <CardActionArea
+            sx={{flexGrow: 1, flexDirection: "column", alignItems: "stretch", height: "100%", width: "100%"}}
             style={{
               imageRendering: "pixelated",
             }}
-          />
-          <ImageListItemBar
-            title={roomName}
-            subtitle={roomId}
-            sx={{
-              fontSize: 26,
-              background: "linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0))",
-            }}
-          />
-        </CardActionArea>
-      </Link>
-    </Card>
-  </Grid>
-);
+          >
+            <CardMedia placeholder={"Loading"} component={AspectBox}>
+              <Image
+                unoptimized
+                layout="fill"
+                src={image}
+                alt={`Thumbnail for room ${roomName}`}
+                style={{
+                  imageRendering: "pixelated",
+                }}
+              />
+            </CardMedia>
+            <ImageListItemBar
+              title={roomName}
+              subtitle={roomId}
+              sx={{
+                fontSize: 26,
+                background: "linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0))",
+              }}
+            />
+          </CardActionArea>
+        </Link>
+      </Card>
+    </Grid>
+  );
+};
